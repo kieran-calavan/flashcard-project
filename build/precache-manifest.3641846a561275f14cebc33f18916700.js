778fdbcb0fb573f4071782e10e74d1e98c52583c
kieran-calavan <93680051+kieran-calavan@users.noreply.github.com>
2022-06-22 22:11:16 -0500

    updates

---
+++ build/precache-manifest.3641846a561275f14cebc33f18916700.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "89f642d6395c2cede9e5",
+    "url": "/static/js/main.89f642d6.chunk.js"
+  },
+  {
+    "revision": "a56506c293bf92f6728d",
+    "url": "/static/js/1.a56506c2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "100295752d3d63e35fcd14749183b4f5",
+    "url": "/index.html"
+  }
+];
